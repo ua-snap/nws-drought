d365aab33ad7a3f212c3796570127e7d95c01bd2
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-09-21 11:39:48 -0700

    I fixed it!!  :)

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,7 @@
 import os
 
 debug = os.getenv("NWS_DROUGHT_DEBUG", False)
-DEBUG_MODE = False if (debug is False or debug != "False") else True
+DEBUG_MODE = False if (debug is False or debug == "False") else True
 dl_loc = os.getenv("NWS_DROUGHT_DOWNLOAD_DIR", False)
 DOWNLOAD_DIR = dl_loc if dl_loc is not False else "/tmp/nws_drought/"
 DATA_LAG_TIME_DAYS = 8
