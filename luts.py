29bacff637bd74f994720aff1fffa6350cb2791a
kyleredilla <kmredilla@alaska.edu>
2022-10-27 18:02:16 -0800

    add luts for lookups

---
+++ luts.py
@@ -0,0 +1,7 @@
+"""Lookup tables for processing the drought indices"""
+
+varname_prefix_lu = {
+    "tp": "total_precipitation",
+    "sd": "snow_depth",
+    "swvl1": "volumetric_soil_water_layer_1",
+}
