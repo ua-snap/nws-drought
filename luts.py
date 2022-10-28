2f296e59764b8a03ae833782b257a970fce06c87
kyleredilla <kmredilla@alaska.edu>
2022-10-28 12:45:57 -0800

    include conversion of swe to cm and include lookup for indices

--- luts.py
+++ luts.py
@@ -5,3 +5,10 @@ varname_prefix_lu = {
     "sd": "snow_depth",
     "swvl1": "volumetric_soil_water_layer_1",
 }
+
+index_lu = {
+    "tp": "Total precipitation",
+    "pntp": "Total precipitation % of Normal",
+    "swe": "Snow water equivalent",
+    "pnswe": "Snow water equivalent % of Normal",
+}
