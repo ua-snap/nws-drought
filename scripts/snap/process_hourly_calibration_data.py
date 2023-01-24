39e84e0f55654dfb9f6adbc784a07ffa3966958e
kyleredilla <kmredilla@alaska.edu>
2023-01-24 10:12:55 -0900

    downgrade netcdf4 so dask tasks work properly

--- scripts/snap/process_hourly_calibration_data.py
+++ scripts/snap/process_hourly_calibration_data.py
@@ -7,23 +7,27 @@ Note: this script is designed to be run from within the parent directory of the 
 """
 
 import argparse
+import os
 import time
 from pathlib import Path
 import xarray as xr
-from dask.distributed import client
+import dask
+from dask.distributed import Client
 from dask.diagnostics import ProgressBar
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-v", dest="varname", type=str, help="Variable name, either 'tp' or 'pev'")
+    parser.add_argument("-d", dest="hourly_dir", type=str, help="Hourly data directory")
     args = parser.parse_args()
     varname = args.varname
+    hourly_dir = Path(args.hourly_dir)
     
     client = Client(n_workers=16)
 
     # assumes running from parent of download dirs
-    fps = sorted(list(Path(f"era5_hourly_{varname}_1981_2020").glob("*.nc")))
+    fps = sorted(hourly_dir.glob("*.nc"))
     ds = xr.open_mfdataset(fps, parallel=True, chunks={"time": -1})
     
     # assumes both pev and tp should be accumulated over a day
@@ -34,5 +38,6 @@ if __name__ == "__main__":
     delayed_write = new_ds.to_netcdf(f"era5_daily_{varname}_1981_2020.nc", compute=False)
     with ProgressBar():
         results = delayed_write.compute()
-        
+    
+    ds.close()
     client.close()
