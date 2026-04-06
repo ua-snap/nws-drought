bb1bd45290def41ca4e4f20d6b6f4c7b7c15e31a
Charlie Parr <cparr4@chinook03.rcs.alaska.edu>
2026-04-06 14:12:14 -0800

    point at `derived-era5-single-levels-daily-statistics`

--- scripts/snap/download_calibration_data.py
+++ scripts/snap/download_calibration_data.py
@@ -1,6 +1,6 @@
-"""This is the script that was used to download the hourly ERA5 potential evaporation data for calibration in SPI and SPEI calcultions. A combined versions of the downloaded data will be provided alongside the other fixed inputs (e.g. climatoglogies). This is provided here for reference and should not be needed by NWS/NOAA.
+"""Download the daily summary ERA5-Land data for calibration in SPI and SPEI.
 
-Usage: 
+Usage:
     python download_calibration_data.py -v pev
     python download_calibration_data.py -v tp
 """
@@ -20,38 +20,42 @@ long_name_lu = {"pev": "potential_evaporation", "tp": "total_precipitation"}
 
 
 def download_calibration_data(long_name, download_dir):
-    """Download potential evaporation for the climatology period of 1981-2020"""
+    """Download ERA5-Land for the climatology period of 1981-2020"""
+    c = cdsapi.Client()
     for year in range(1981, 2021):
-        logging.info(f"Downloading hourly ERA5 PEV data for {year} to {download_dir}")
-        c = cdsapi.Client()
-        c.retrieve(
-            "reanalysis-era5-single-levels",
-            {
-                "product_type": "reanalysis",
-                "format": "netcdf",
-                "variable": long_name,
-                "year": str(year),
-                "month": [str(month).zfill(2) for month in range(1, 13)],
-                "day": [str(day).zfill(2) for day in range(1, 32)],
-                "time": [
-                    "0" + str(x) + ":00" if x <= 9 else str(x) + ":00"
-                    for x in range(0, 24)
-                ],
-                "area": DL_BBOX,
-            },
-            download_dir.joinpath(f"{long_name}_hourly_{year}.nc"),
-        )
-    
+        for month in [str(month).zfill(2) for month in range(1, 13)]:
+            logging.info(
+                f"Downloading ERA5-Land {long_name} data for {month}-{year} to {download_dir}"
+            )
+            c.retrieve(
+                "derived-era5-single-levels-daily-statistics",
+                {
+                    "product_type": "reanalysis",
+                    "variable": long_name,
+                    "year": str(year),
+                    "month": month,
+                    "day": [str(day).zfill(2) for day in range(1, 32)],
+                    "area": DL_BBOX,
+                    "daily_statistic": "daily_sum",
+                    "time_zone": "utc+00:00",
+                    "frequency": "1_hourly",
+                    "data_format": "netcdf",
+                },
+                download_dir.joinpath(f"{long_name}_daily_{year}_{month}.nc"),
+            )
+
     return
 
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-v", dest="varname", type=str, help="Variable name, either 'tp' or 'pev'")
+    parser.add_argument(
+        "-v", dest="varname", type=str, help="Variable name, either 'tp' or 'pev'"
+    )
     args = parser.parse_args()
     varname = args.varname
     long_name = long_name_lu[varname]
     logging.basicConfig(level=logging.INFO)
-    download_dir = Path(f"era5_hourly_{varname}_1981_2020")
+    download_dir = Path(f"era5_daily_{varname}_1981_2020")
     download_dir.mkdir(exist_ok=True)
     download_calibration_data(long_name, download_dir)
